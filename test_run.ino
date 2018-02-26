ce70a4882f1a6a64fa18daa667d21d5d979d862c
Amir-Omidfar <omidfar73@gmail.com>
2018-02-25 20:17:13 -0800

    Add files via upload
    This piece of code have measurements from all three sensors and moves both servo motors by some pre defined controller

---
+++ test_run.ino
@@ -0,0 +1,381 @@
+/*VL53L0X Duel Sensor Test Code
+ * By: Kevin Pololu and steinerlein
+ * Original Source: https://github.com/pololu/vl53l0x-arduino/issues/1
+ * Edited for EE183DA by Nathan Pilbrough
+ * 
+ * Description: Basic code to test the functionality of two
+ * VL53L0X sensors operating at the same time. Refer to the 
+ * startup guide on CCLE for more information
+ */
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <Servo.h>
+
+Servo servo_left;
+Servo servo_right;
+int counter=0;
+int stop_position=90; //For the 360 degree servo,
+                      //0 goes full speed clockwise; 
+                      //90 stop spinning ;
+                      //180 goes full speed counterclockwise.
+
+int friction=7; // If the surface is smooth, make this value greater.
+
+
+////////////////////////////Servo test function 
+
+
+//Control Both Wheels
+void moving_forward(int velocity,int duration)
+{
+  servo_left.write(stop_position+velocity);
+  servo_right.write(stop_position-velocity-4);
+  delay(duration);
+  }
+  
+void moving_backward(int velocity,int duration)
+{
+  servo_left.write(stop_position-velocity);
+  servo_right.write(stop_position+velocity);
+  delay(duration);
+  }
+
+ void stop_the_car_forward(int duration)
+{
+  servo_left.write(stop_position);
+  delay(30);
+  servo_right.write(stop_position);
+  delay(duration);
+  }
+   void stop_the_car(int duration)
+{
+  servo_left.write(stop_position);
+  servo_right.write(stop_position);
+  delay(duration);
+  }
+//void turn_left(int degree)
+
+//Control a Single Wheel
+void moving_forward_right_wheel(int velocity)
+{
+  servo_right.write(stop_position-velocity);
+  
+  }
+void moving_forward_left_wheel(int velocity)
+{
+  servo_left.write(stop_position+velocity);
+  
+  }
+void moving_backward_right_wheel(int velocity)
+{
+  servo_right.write(stop_position+velocity);
+ 
+  }
+ void moving_backward_left_wheel(int velocity)
+{
+  servo_left.write(stop_position-velocity);
+  
+  }
+  void stop_right_wheel()
+{
+  servo_right.write(stop_position);
+
+  }
+   void stop_left_wheel()
+{
+  servo_left.write(stop_position);
+  
+  }
+
+
+
+
+//////////////////////////////////////////////////////
+
+#define SDA_PORT 14
+#define SCL_PORT 12
+VL53L0X sensor;
+VL53L0X sensor2;
+
+
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+float dest1[3] = {0,0,0};
+float dest2[3] = {0,0,0};
+void calibration(float * dest1, float * dest2) 
+ {
+ uint16_t ii = 0, sample_count = 0;
+ int32_t mag_bias[3] = {0, 0, 0}, mag_scale[3] = {0, 0, 0};
+ int16_t mag_max[3] = {-32767, -32767, -32767}, mag_min[3] = {32767, 32767, 32767}, mag_temp[3] = {0, 0, 0};
+
+ Serial.println("Mag Calibration: Wave device in a figure eight until done!");
+ delay(3000);
+
+// shoot for ~fifteen seconds of mag data
+sample_count = 1500;
+for(ii = 0; ii < sample_count; ii++) {
+  Serial.println(ii);
+  // Request first magnetometer single measurement
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+  
+  // Read register Status 1 and wait for the DRDY: Data Ready
+  uint8_t ST1;
+  do
+  {
+    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+  }
+  while (!(ST1&0x01));
+
+  // Read magnetometer data  
+  uint8_t Mag[7];  
+  I2Cread(MAG_ADDRESS,0x03,7,Mag);
+  // Create 16 bits values from 8 bits data
+  // Magnetometer
+  int16_t mx=(Mag[1]<<8 | Mag[0]);
+  int16_t my=(Mag[3]<<8 | Mag[2]);
+  int16_t mz=(Mag[5]<<8 | Mag[4]);
+  mag_temp[0] = mx;
+  mag_temp[1] = my;
+  mag_temp[2] = mz;
+for (int jj = 0; jj < 3; jj++) {
+  if(mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
+  if(mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
+}
+
+}
+
+
+Serial.println("finish for loop");
+// Get hard iron correction
+ mag_bias[0]  = (mag_max[0] + mag_min[0])/2;  // get average x mag bias in counts
+ mag_bias[1]  = (mag_max[1] + mag_min[1])/2;  // get average y mag bias in counts
+ mag_bias[2]  = (mag_max[2] + mag_min[2])/2;  // get average z mag bias in counts
+Serial.println(mag_max[0]);
+ dest1[0] = mag_bias[0];
+ dest1[1] = mag_bias[1];
+ dest1[2] = mag_bias[2];
+   
+// Get soft iron correction estimate
+ mag_scale[0]  = (mag_max[0] - mag_min[0])/2;  // get average x axis max chord length in counts
+ mag_scale[1]  = (mag_max[1] - mag_min[1])/2;  // get average y axis max chord length in counts
+ mag_scale[2]  = (mag_max[2] - mag_min[2])/2;  // get average z axis max chord length in counts
+
+ float avg_rad = mag_scale[0] + mag_scale[1] + mag_scale[2];
+ avg_rad /= 3.0;
+
+ dest2[0] = avg_rad/((float)mag_scale[0]);
+ dest2[1] = avg_rad/((float)mag_scale[1]);
+ dest2[2] = avg_rad/((float)mag_scale[2]);
+ Serial.println("Mag Calibration done!");  
+}
+
+
+void setup()
+{
+
+  //
+  servo_left.attach(D1);
+  servo_right.attach(D2);
+  //
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  digitalWrite(D7, LOW);
+  digitalWrite(D8, LOW);
+
+  delay(500);
+  Wire.begin(SDA_PORT,SCL_PORT);
+
+  Serial.begin (115200);
+
+  digitalWrite(D3, HIGH);
+  delay(150);
+  Serial.println("00");
+  
+  sensor.init(true);
+  Serial.println("01");
+  delay(100);
+  sensor.setAddress((uint8_t)22);
+
+  digitalWrite(D4, HIGH);
+  delay(150);
+  sensor2.init(true);
+  Serial.println("03");
+  delay(100);
+  sensor2.setAddress((uint8_t)25);
+  Serial.println("04");
+
+  Serial.println("addresses set");
+  
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  Wire.begin(SDA_PORT,SCL_PORT);
+  
+   // Set by pass mode for the magnetometers
+  I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
+  
+  // Request first magnetometer single measurement
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+  for (byte i = 1; i < 120; i++)
+  {
+
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+    } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+
+  delay(3000);
+}
+
+long int cpt=0;
+void loop()
+{
+   // _______________
+  // ::: Counter :::
+  
+  // Display data counter
+  Serial.print (cpt++,DEC);
+  Serial.print ("\t");
+  if (cpt==1) {
+  calibration(dest1, dest2);
+  for (int i = 0; i<3; i++) {
+    Serial.print(dest1[i]);
+    Serial.print('/t');
+    Serial.print(dest2[i]);
+    Serial.println("");
+  }
+  }
+  // _____________________
+  // :::  Magnetometer ::: 
+
+  // Request first magnetometer single measurement
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x01);
+  
+  // Read register Status 1 and wait for the DRDY: Data Ready
+  
+  uint8_t ST1;
+  do
+  {
+    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+  }
+  while (!(ST1&0x01));
+
+  // Read magnetometer data  
+  uint8_t Mag[7];  
+  I2Cread(MAG_ADDRESS,0x03,7,Mag);
+
+  // Create 16 bits values from 8 bits data
+  
+  // Magnetometer
+  int16_t mx=(Mag[1]<<8 | Mag[0]);
+  int16_t my=(Mag[3]<<8 | Mag[2]);
+  int16_t mz=(Mag[5]<<8 | Mag[4]);
+  mx = dest2[0] * (mx - dest1[0]);
+  my = dest2[1] * (my - dest1[1]);
+  mz = dest2[2] * (mz - dest1[2]);
+  float heading = atan2(mx, my);
+
+  // Once you have your heading, you must then add your 'Declination Angle',
+  // which is the 'Error' of the magnetic field in your location. Mine is 0.0404 
+  // Find yours here: http://www.magnetic-declination.com/
+  
+  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
+  float declinationAngle = 0.0404;
+  heading += declinationAngle;
+
+  // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/PI; 
+  Serial.print("Theta:  ");
+  Serial.print(headingDegrees);
+  Serial.print("\t");
+  Serial.print("lx: ");
+  Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.print("ly: ");
+  Serial.println(sensor2.readRangeSingleMillimeters());
+
+  Serial.print("counter: ");
+  Serial.println(counter);
+  ///////////////////////////////////////////////Servo test
+  if (counter == 5)
+  {
+    
+moving_forward(20,1000);
+stop_the_car_forward(1000);
+
+moving_forward_right_wheel(40);
+moving_backward_left_wheel(40);
+delay(friction*45);
+stop_the_car(1000);
+
+moving_forward(20,1000);
+stop_the_car_forward(1000);
+  }
+  
+  //////////////////////////////////////////////////////////
+
+
+  
+  if (sensor.timeoutOccurred()) { Serial.println(" TIMEOUT"); }
+  delay(1000);
+
+  counter++;
+}
+
+// This function read Nbytes bytes from I2C device at address Address. 
+// Put read bytes starting at register Register in the Data array. 
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+  
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes); 
+  uint8_t index=0;
+  while (Wire.available())
+    Data[index++]=Wire.read();
+}
+
+
+// Write a byte (Data) in device (Address) at register (Register)
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
