76f3a82cbff78dd4d087819ffbca338b43295df0
Amir-Omidfar <omidfar73@gmail.com>
2018-03-06 16:27:01 -0800

    Add files via upload

---
+++ CommunicationDweet/ESP/ESPWritecode.ino
@@ -0,0 +1,90 @@
+/***************************************************
+
+Credit to  Marco Schwartz for Open Home Automation.
+BSD license, all text above must be included in any redistribution
+
+Based on the original sketches supplied with the ESP8266/Arduino
+implementation written by Ivan Grokhotkov
+
+****************************************************/
+
+// Libraries
+#include <ESP8266WiFi.h>
+
+// WiFi parameters
+const char* ssid = "UCLA_WEB";
+const char* password = "";
+
+
+
+
+
+// Host
+const char* host = "dweet.io";
+
+void setup() {
+
+  // Start Serial
+  Serial.begin(115200);
+  delay(10);
+
+  // Init DHT
+  //dht.begin();
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  Serial.print("Connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // Reading temperature and humidity
+  int h = 50;//dht.readHumidity();
+  // Read temperature as Celsius
+  int t = 40;//dht.readTemperature();
+
+  // This will send the request to the server
+  client.print(String("GET /dweet/for/test12?data1=") + String(t) + "&data2=" + String(h) + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+     /*client.print(String ("GET /get/latest/dweet/for/2wheelcar")+" HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");*/
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  // Repeat every 10 seconds
+  delay(10000);
+
+}
